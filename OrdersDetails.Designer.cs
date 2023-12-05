1e6c4ff15679797d82963e33b6f9f3584ccc8712
fatima2255 <64276992+fatima2255@users.noreply.github.com>
2023-12-05 07:53:10 +0500

    Update Orders Placed by customers
    The admin can confirm customers orders and update them

---
+++ OrdersDetails.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace E_mart_Project.Admin_Auth
+{
+    partial class OrdersDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OrdersDetails));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Confirm = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.83224F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.16776F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1169, 628);
+            this.tableLayoutPanel1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(15)))), ((int)(((byte)(100)))));
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(424, 622);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(433, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 3;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.91689F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.86214F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.220957F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(733, 622);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Confirm});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(15)))), ((int)(((byte)(100)))));
+            this.dataGridView1.Location = new System.Drawing.Point(3, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(727, 490);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Confirm
+            // 
+            this.Confirm.HeaderText = "Deliver Order";
+            this.Confirm.MinimumWidth = 6;
+            this.Confirm.Name = "Confirm";
+            this.Confirm.Text = "Confirm";
+            this.Confirm.UseColumnTextForButtonValue = true;
+            this.Confirm.Width = 125;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 4;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 1.208459F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.90936F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47.50378F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.33132F));
+            this.tableLayoutPanel3.Controls.Add(this.button1, 3, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 573);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.84337F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89.15662F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(727, 46);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(15)))), ((int)(((byte)(100)))));
+            this.button1.Location = new System.Drawing.Point(573, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(15)))), ((int)(((byte)(100)))));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(727, 74);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "View Cart Items";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OrdersDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 628);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "OrdersDetails";
+            this.Text = "OrdersDetails";
+            this.Load += new System.EventHandler(this.OrdersDetails_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewButtonColumn Confirm;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ OrdersDetails.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace E_mart_Project.Admin_Auth
+{
+    public partial class OrdersDetails : Form
+    {
+        int OrderID, contact;
+        string Fname, home, Pname;
+        public OrdersDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) // back
+        {
+            this.Hide();
+            Form f = new Options_A();
+            f.Show();
+        }
+
+        private void OrdersDetails_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            dataGridView1.ReadOnly = true;
+
+            int totalRowHeight;
+
+            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            totalRowHeight = dataGridView1.ColumnHeadersHeight;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                totalRowHeight += row.Height;
+
+            var con = Configuration.getInstance().getConnection();
+            //SqlCommand cmd = new SqlCommand("select OP.OrderId, P.FirstName, P.Contact, P.Home, Pro.Name from OrdersPlaced as OP join Person as P on P.Id = OP.Person_Id join Cart as C on P.Id = C.Person_Id join CartItems as CI on CI.Cart_ID = C.CartId join Products as Pro on Pro.ProductId = CI.Product_ID where OP.Order_Status = 8", con);
+            SqlCommand cmd = new SqlCommand("select * from OrdersPlaced where Order_Status != 8", con);
+            cmd.Connection = con;
+            SqlDataReader sqlData = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(sqlData);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex >= 0 && e.ColumnIndex < dataGridView1.Columns.Count && e.RowIndex >= 0)
+            {
+                var clickedColumn = dataGridView1.Columns[e.ColumnIndex];
+
+                // Check if the clicked column is the button column
+                if (clickedColumn != null && clickedColumn.Name == "Confirm")
+                {
+                    //get values in variables 
+                    OrderID = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString());
+                    Fname = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+                    contact = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString());
+                    home = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+                    Pname = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+
+                    UpdateStatus();
+
+                }
+            }
+        }
+
+        public void UpdateStatus()
+        {
+            int oid = 0, pid = 0, ostatus = 8,cid = 0;
+            DateTime date = DateTime.Now;
+            var con1 = Configuration.getInstance().getConnection();
+            
+            SqlCommand cmd = new SqlCommand("SELECT OrderId, Person_Id, Cart_Id, DateofOrder, Order_Status from OrdersPlaced", con1);
+            
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+              
+                    oid = reader.GetInt32(0); 
+                    pid = reader.GetInt32(1);
+                    cid = reader.GetInt32(2);
+                    date = reader.GetDateTime(3);
+                    ostatus = reader.GetInt32(4);
+                            
+                }
+            }
+
+            var con2 = Configuration.getInstance().getConnection();
+
+            SqlCommand cmd2 = new SqlCommand($"update OrdersPlaced set Person_Id = @pid, Cart_Id = @cid, DateofOrder = @date, Order_Status = @status where OrderId = @oid", con2);
+            cmd2.Parameters.AddWithValue("@oid", oid); 
+            cmd2.Parameters.AddWithValue("@pid", pid);
+            cmd2.Parameters.AddWithValue("@cid", cid);
+            cmd2.Parameters.AddWithValue("@date", date);
+            cmd2.Parameters.AddWithValue("@status", 9);
+
+            cmd2.ExecuteNonQuery(); // doesnt update
+
+            MessageBox.Show("Order confirmed");
+            reload();
+
+        }
+
+        public void reload()
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            dataGridView1.ReadOnly = true;
+
+            int totalRowHeight;
+
+            dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            totalRowHeight = dataGridView1.ColumnHeadersHeight;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+                totalRowHeight += row.Height;
+
+            var con = Configuration.getInstance().getConnection();
+            //SqlCommand cmd = new SqlCommand("select OP.OrderId, P.FirstName, P.Contact, P.Home, Pro.Name from OrdersPlaced as OP join Person as P on P.Id = OP.Person_Id join Cart as C on P.Id = C.Person_Id join CartItems as CI on CI.Cart_ID = C.CartId join Products as Pro on Pro.ProductId = CI.Product_ID where OP.Order_Status = 8", con);
+            SqlCommand cmd = new SqlCommand("select * from OrdersPlaced", con);
+
+            cmd.Connection = con;
+            SqlDataReader sqlData = cmd.ExecuteReader();
+            DataTable dataTable = new DataTable();
+            dataTable.Load(sqlData);
+            dataGridView1.DataSource = dataTable;
+        }
+
+    }
+}
